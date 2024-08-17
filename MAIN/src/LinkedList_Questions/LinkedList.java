f61dbbb5d14b681794c2ffb3c48c3b6f9ed8f673
Ayush <akagarwal.111a@gmail.com>
2024-08-17 15:01:11 +0530

    Remove a cycle in LinkedList

--- MAIN/src/LinkedList_Questions/LinkedList.java
+++ MAIN/src/LinkedList_Questions/LinkedList.java
@@ -231,6 +231,35 @@ public class LinkedList {
         return false;
     }
 
+    public static void removeCycle() {
+        // detect cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+        while (fast!=null && fast.next!=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow==fast) {
+                cycle = true;
+                break;
+            }
+        }
+        if (cycle == false) {
+            return;
+        }
+
+        //finding meeting point
+        slow = head;
+        Node prev = null;
+        while (slow!=fast) {
+            prev = fast;
+            slow = slow.next;
+            fast= fast.next;
+        }
+        //remove cycle
+        prev.next = null;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.print();
@@ -261,6 +290,15 @@ public class LinkedList {
         ll.print();
         System.out.println(ll.checkPalindrome());
         ll.print();
+        System.out.println(isCycle());
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp;
+        //1->2->3->2
+        System.out.println(isCycle());
+        removeCycle();
         System.out.println(isCycle());
     }
 
