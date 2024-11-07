1afe5889ec70b7a09e93749876f3230c5e61793f
Ayush <akagarwal.111a@gmail.com>
2024-11-07 17:35:02 +0530

    Queue Using Java Collection Framework (JCF).

---
+++ MAIN/src/Queues_Questions/Queue_Using_JCF.java
@@ -0,0 +1,19 @@
+package Queues_Questions;
+
+import java.util.*;
+
+public class Queue_Using_JCF {
+
+    public static void main(String[] args) {
+        //Queue q = new Queue();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.err.println(q.peek());
+            q.remove();
+        }
+    }
+}
