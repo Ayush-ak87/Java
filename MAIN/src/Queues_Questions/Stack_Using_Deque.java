b253f40195c0a7594f77064874549c0ca8bed200
Ayush <akagarwal.111a@gmail.com>
2024-12-17 18:36:30 +0530

    Stack Using Deque

---
+++ MAIN/src/Queues_Questions/Stack_Using_Deque.java
@@ -0,0 +1,28 @@
+package Queues_Questions;
+import java.util.*;
+
+public class Stack_Using_Deque {
+    static class Stack {
+        Deque<Integer> dq = new LinkedList<>();
+
+        public void push(int data) {
+            dq.addLast(data);
+        }
+        public int pop() {
+            return dq.removeLast();
+        }
+        public int peek() {
+            return dq.getLast();
+        }
+    }
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        System.out.println("Peek = "+ s.peek());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+    }
+}
